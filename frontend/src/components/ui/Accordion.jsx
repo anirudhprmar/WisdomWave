a2e10b5fd534641c4ae43d79597ebf3bba7b975d
Anirudh Parmar <anirudhparmar2004@gmail.com>
2025-04-21 13:56:51 +0530

    feat: Update Accordion component to apply theme-based styling for improved visual consistency

--- frontend/src/components/ui/Accordion.jsx
+++ frontend/src/components/ui/Accordion.jsx
@@ -1,11 +1,14 @@
+import { useThemeStore } from "../../store/useThemeStore";
+
 function Accordion({question,answer}) {
-    
+  
+  const {theme} = useThemeStore();
   return (
     <div>
       <div className="collapse collapse-plus bg-base-100 border border-base-300">
   <input type="radio" name="my-accordion-3" defaultChecked />
-  <div className="collapse-title font-semibold">{question}</div>
-  <div className="collapse-content text-sm">{answer}</div>
+  <div className={theme === 'light' ? "collapse-title font-semibold text-gray-700" : "collapse-title font-semibold"}>{question}</div>
+  <div className={theme === 'light' ? "collapse-content text-sm text-gray-700" : "collapse-content text-sm"}>{answer}</div>
 </div>
     </div>
   )
